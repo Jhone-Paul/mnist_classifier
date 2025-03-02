c4cccff5c26205be0fa56ccc96f6ddccdd2bd433
John <26047888@sun.ac.za>
2025-03-02 16:09:10 +0200

    added backpropagation

--- cnn/cnn.py
+++ cnn/cnn.py
@@ -25,19 +25,59 @@ def forward(image, label):
 
 print('MNIST CNN initialized!')
 
+def train(im, label, lr=.005):
+    '''
+    Completes a full training step on the given image and label.
+    Returns the cross-entropy loss and accuracy.
+    - image is a 2d numpy array
+    - label is a digit
+    - lr is the learning rate
+    '''
+    # Forward
+    out, loss, acc = forward(im, label)
+
+    # Calculate initial gradient
+    gradient = np.zeros(10)
+    gradient[label] = -1 / out[label]
+
+    # Backprop
+    gradient = softmax.backprop(gradient, lr)
+    gradient = pool.backprop(gradient)
+    gradient = conv.backprop(gradient, lr)
+
+    return loss, acc
+
+print('MNIST CNN initialized!')
+
+# Train!
 loss = 0
 num_correct = 0
-for i, (im, label) in enumerate(zip(test_images, test_labels)):
-  # Do a forward pass.
-  _, l, acc = forward(im, label)
-  loss += l
-  num_correct += acc
+for i, (im, label) in enumerate(zip(train_images, train_labels)):
+    if i >= 1000:
+        break
+    if i % 100 == 99:
+        print(
+        '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
+        (i + 1, loss / 100, num_correct)
+        )
+        loss = 0
+        num_correct = 0
 
-  # Print stats every 100 steps.
-  if i % 100 == 99:
-    print(
-      '[Step %d] Past 100 steps: Average Loss %.3f | Accuracy: %d%%' %
-      (i + 1, loss / 100, num_correct)
-    )
-    loss = 0
-    num_correct = 0
+    l, acc = train(im, label)
+    loss += l
+    num_correct += acc
+
+print('\n--- Testing the CNN ---')
+loss = 0
+num_correct = 0
+num_tests = 10000
+for im, label in zip(test_images, test_labels):
+    if i >= num_tests:
+        break
+    _, l, acc = forward(im, label)
+    loss += l
+    num_correct += acc
+
+
+print('Test Loss:', loss / num_tests)
+print('Test Accuracy:', num_correct / num_tests)
--- cnn/conv.py
+++ cnn/conv.py
@@ -13,6 +13,9 @@ class Conv3x3:
                 im_region = image[i:(i+3), j:(j+3)]
                 yield im_region,i,j
     def forward(self, input):
+
+        self.last_input = input
+
         h, w = input.shape
         output = np.zeros((h-2,w-2,self.num_filters))
 
@@ -21,3 +24,20 @@ class Conv3x3:
             output[i,j] =np.sum(im_region *self.filters, axis =(1,2))
 
         return output
+
+    def backprop(self, d_L_d_out, learn_rate):
+        
+        d_L_d_filters = np.zeros(self.filters.shape)
+
+        for im_region, i, j in self.iterate_regions(self.last_input):
+            for f in range(self.num_filters):
+                d_L_d_filters[f] += d_L_d_out[i, j, f] * im_region
+
+        # Update filters
+        self.filters -= learn_rate * d_L_d_filters
+
+        # We aren't returning anything here since we use Conv3x3 as
+        # the first layer in our CNN. Otherwise, we'd need to return
+        # the loss gradient for this layer's inputs, just like every
+        # other layer in our CNN.
+        return None
